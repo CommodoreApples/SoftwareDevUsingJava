e83756205c54d3cebe7ddffcfb3823a3398dccf2
Alexander Applegate <43449402+Konikei@users.noreply.github.com>
2024-01-21 19:30:46 -0500

    Programming Assignments

---
+++ PA1.java
@@ -0,0 +1,12 @@
+public class UnitConverter {
+
+    /** Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        return foot * 0.3048;
+    }
+
+    /** Convert from meters to feet */
+    public static double meterToFoot(double meter) {
+        return meter / 0.3048;
+    }
+}
